7516d4db72613d0fc6196654e3c464afecdeb511
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-07-16 00:47:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function x(){
+	return new Promise((resolve, reject)=>
+		{
+		setTimeout(() =>{
+			resolve("Hello, world!");
+		},1000);
+	})
+}
+let y = x();
+y.then((data) =>{
+	const op = document.getElementById("output");
+	op.innerText = data;
+	console.log(data);
+});
